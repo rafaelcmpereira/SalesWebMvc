8d5e4ef8dfe48d59b979b3b0f75180914814d42e
Rafael Carregalo Martin Pereira <rafaelcmpereira@gmail.com>
2021-06-17 12:55:39 -0300

    Project with all layers

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required.")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "{0} required.")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required.")]
+        [Display(Name = "Brith Date")]
+        [DataType(DataType.Date)]
+        public DateTime BrithDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat (DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required.")]
+        [Range(100.00, 50000.00, ErrorMessage ="{0} must be from {1} to {2}")]
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        [Display(Name = "Department")]
+        public int  DepartmentId { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, DateTime brithDate, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BrithDate = brithDate;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            Sales.Add(salesRecord);
+        }
+
+        public void RemoveSaller(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);
+        }
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sales => sales.Date >= initial || sales.Date <= final)
+                .Sum(sales => sales.Amount);
+        }
+    }
+}
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,71 @@
+﻿using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Services.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Seller>> FindAllAsync()
+        {
+            return await _context.Seller.ToListAsync();
+        }
+
+        public async Task InsertAsync(Seller seller)
+        {
+            _context.Add(seller);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Seller> FindByIdAsync(int id)
+        {
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+
+                throw new IntegrityException("The seller cannot be deleted because it has sales");
+            }
+
+        }
+        public async Task UpdateAsync(Seller seller)
+        {
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == seller.Id);
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(seller);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+        }
+    }
+}
